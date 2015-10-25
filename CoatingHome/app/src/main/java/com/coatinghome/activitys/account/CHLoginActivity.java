9f8dea8de5100e685f4835a06849eab05ca8c4c2
wuyajun <1990887586@qq.com>
2015-10-25 21:24:13 +0800

    login code

--- CoatingHome/app/src/main/java/com/coatinghome/activitys/account/CHLoginActivity.java
+++ CoatingHome/app/src/main/java/com/coatinghome/activitys/account/CHLoginActivity.java
@@ -1,6 +1,8 @@
 package com.coatinghome.activitys.account;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -23,8 +25,19 @@ public class CHLoginActivity extends CHBaseActivity {
 
     @InjectView(R.id.pub_title_view)
     private LinearLayout mPubTitleView;
-    @InjectView(R.id.public_center_title)
-    private TextView mCenterTitle;
+
+    @InjectView(R.id.login_input_account)
+    private EditText mLoginInputAccount;
+    @InjectView(R.id.login_input_pw)
+    private EditText mLoginInputPw;
+
+    @InjectView(R.id.login_forget_pw_text)
+    private TextView mLoginFogetPw;
+    @InjectView(R.id.login_register_text)
+    private TextView mLoginRegister;
+
+    @InjectView(R.id.login_button)
+    private TextView mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,21 +49,47 @@ public class CHLoginActivity extends CHBaseActivity {
 
     private void initViews() {
         mPubTitleView.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        mCenterTitle.setText("登录");
-        mCenterTitle.setTextColor(getResources().getColor(R.color.colorGray1));
-        CHContrat.showView( mCenterTitle);
+
+        mLoginFogetPw.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        mLoginFogetPw.getPaint().setAntiAlias(true);//抗锯齿
+        mLoginFogetPw.setOnClickListener(this);
+        mLoginRegister.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        mLoginRegister.getPaint().setAntiAlias(true);//抗锯齿
+        mLoginRegister.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.login_button:
+                if (checkRegisterContent()) return;
+                break;
             default:
                 break;
         }
     }
 
-    private boolean checkRegisterContent() {
+    private String account;
+    private String pwd;
 
-        return false;
+    private boolean checkRegisterContent() {
+        account = mLoginInputAccount.getText().toString();
+        pwd = mLoginFogetPw.getText().toString();
+        if (TextUtils.isEmpty(account)) {
+            mLoginInputAccount.setError("帐号不能为空哟！");
+            return false;
+        } else if (!CHContrat.isMobileNO(account)) {
+            mLoginInputAccount.setError("帐号格式不正确！");
+            return false;
+        }
+        if (TextUtils.isEmpty(pwd)) {
+            mLoginFogetPw.setError("密码不能为空哟！");
+            return false;
+        } else if (pwd.length() < 6) {
+            mLoginFogetPw.setError("密码错误！");
+            return false;
+        }
+
+        return true;
     }
 }
--- CoatingHome/app/src/main/java/com/coatinghome/providers/CHContrat.java
+++ CoatingHome/app/src/main/java/com/coatinghome/providers/CHContrat.java
@@ -42,6 +42,21 @@ public class CHContrat {
         }
     }
 
+    /**
+     * 验证手机格式
+     */
+    public static boolean isMobileNO(String mobiles) {
+    /*
+    移动：134、135、136、137、138、139、150、151、157(TD)、158、159、187、188
+    联通：130、131、132、152、155、156、185、186
+    电信：133、153、180、189、（1349卫通）
+    总结起来就是第一位必定为1，第二位必定为3或5或8，其他位置的可以为0-9
+    */
+        String telRegex = "[1][358]\\d{9}";//"[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
+        if (TextUtils.isEmpty(mobiles)) return false;
+        else return mobiles.matches(telRegex);
+    }
+
     //---------------------------------- Test Datas
     public static final String userIcon = "http://diy.qqjay.com/u2/2013/0403/a778f5b0538769ae5e0046a83a4c243c.jpg";
 
